9ec32c3f2dad7a1c108a3bdaa24ce40521f81271
afbayonac <afbayonac@gmail.com>
2020-06-26 22:17:17 -0500

    List Funds

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,20 @@
-axios
-  .get("https://www.grupobancolombia.com/consultarFondosInversion/rest/servicio/buscarInformacionFondo/800227622")
-  .then(res => console.log(res))
+
+/* eslint-disable no-undef */
+
+const main = async () => {
+  const response = await axios.get('./database/db.json')
+  const db = response.data
+  const funds = db.funds
+  const root = d3.select('.app')
+
+  root
+    .selectAll('div')
+    .data(funds)
+    .enter()
+    .append('p')
+    .text((d) => `${d.name} ${d.currentYear[d.currentYear.length - 1].value}`)
+
+  console.log(funds)
+}
+
+main()
